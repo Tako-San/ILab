47129422a6669b155ae5916269f60f2c0deec54f
Tako-San <dxe13@mail.ru>
2020-03-13 14:55:38 +0300

    Map files created

---
+++ MAP/map.h
@@ -0,0 +1,22 @@
+#ifndef MAP
+#define MAP
+
+#include <stdio.h>
+
+
+class Map
+{
+    Node * root;
+
+}
+
+class Node
+{
+    Data val;
+    Node * parent;
+    Node * l_ch;
+    Node * r_ch;
+}
+
+
+#endif //MAP
